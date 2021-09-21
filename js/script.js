0f32ef0bbf9bba4de954244a6de03de2a5a41710
Xavier Boring <tboring2004@gmail.com>
2021-09-21 12:38:30 -0700

    added about me section && updated UI

--- js/script.js
+++ js/script.js
@@ -4,3 +4,14 @@ document.getElementById("hamburger").onclick = function toggleMenu() {
       navToggle.item(i).classList.toggle("hidden");
     }
   };
+
+//  Download resume
+const downloadResume = () => {
+	  const a = document.createElement('a');
+	  a.href = `${window.location.origin}${Resume}`;
+	  // console.log(a.href);
+	  a.download = "JaredsResume.pdf";
+	  document.body.appendChild(a);
+	  a.click();
+	  document.body.removeChild(a);
+}
